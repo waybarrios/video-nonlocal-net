31be5a83244ef46617a488176a71230e54a4b2f0
Wayner Barrios <waybarrios@gmail.com>
2019-01-25 16:23:24 -0500

    updating bash as run file

--- process_data/DIVA/run_createdb.sh
+++ process_data/DIVA/run_createdb.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 # create training lmdb:
-# each row in lmdb: video_file_name, label
-mkdir -p ../../data/lmdb
-mkdir -p ../../data/lmdb/DIVA_lmdb_multicrop
-python ../kinetics/create_video_lmdb.py --dataset_dir ../../data/lmdb/DIVA_lmdb_multicrop/train  --list_file /data/weik/DIVA/divaTrainTestList20/trainlist_shuffle_rep.txt
+mkdir -p /code/nonlocal/data/lmdb
+mkdir -p /code/nonlocal/data/lmdb/DIVA_lmdb_multicrop
+python ../kinetics/create_video_lmdb.py --dataset_dir /code/nonlocal/data/lmdb/DIVA_lmdb_multicrop/train  --list_file /data/divaTrainTestList20/trainlist01_video_shuffle_rep.txt
 
 # create val lmdb:
 # python ../kinetics/create_video_lmdb.py --dataset_dir ../../data/lmdb/DIVA_lmdb_multicrop/val  --list_file /data/weik/DIVA/divaTrainTestList20/vallist.txt
